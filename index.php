201bfd2da09304ae270ae82c8fbbdbb642052112
ethan <ethan36@hotmail.fr>
2026-03-12 13:47:41 +0100

    Replace fraudbg.jpg with freightin.png on Fraud Bank Analysis project

--- index.php
+++ index.php
@@ -222,7 +222,7 @@
           <div class="col-md-4 reveal">
             <a href="fraud.html" class="project-card">
               <div class="project-img">
-                <img src="assets/img/fraudbg.jpg" alt="Fraud Bank Analysis">
+                <img src="assets/img/freightin.png" alt="Fraud Bank Analysis">
                 <div class="project-overlay">
                   <i class="bi bi-arrow-up-right"></i>
                 </div>
